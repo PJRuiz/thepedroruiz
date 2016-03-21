faccdb8b99a5555a471dcaf022ad7101d9e46487
Pedro Ruíz <pedro.ruiz.diaz@me.com>
2016-03-21 10:23:53 +0900

    CyberPunch added

--- js/initialize_piling.js
+++ js/initialize_piling.js
@@ -4,7 +4,7 @@
 $('#pagepiling').pagepiling({
     verticalCentered: false,
     css3: false,
-    sectionsColor: ['white', '#fc0', 'white', 'white'],
+    sectionsColor: ['white', '#fc0', 'gray', '#2586c8', 'white'],
     onLeave: function (index, nextIndex, direction) {
 
         //fading out the txt of the leaving section
@@ -15,7 +15,7 @@ $('#pagepiling').pagepiling({
 
 
         //reaching our last section? The one with our normal site?
-        if (nextIndex == 4) {
+        if (nextIndex == 5) {
             $('#arrow').hide();
 
             //fading out navigation bullets
@@ -27,7 +27,7 @@ $('#pagepiling').pagepiling({
         }
 
         //leaving our last section? The one with our normal site?
-        if (index == 4) {
+        if (index == 5) {
             $('#arrow').show();
 
             //fadding in navigation bullets
